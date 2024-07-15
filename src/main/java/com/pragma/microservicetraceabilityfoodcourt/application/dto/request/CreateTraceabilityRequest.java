1f96212f84a03863014c65b14e42c43a44c04985
Nicolás Olmos <nicolas.olmos@pragma.com.co>
2024-07-15 10:50:19 -0500

    feat[HU-17(traceability)] add dto for creation

---
+++ src/main/java/com/pragma/microservicetraceabilityfoodcourt/application/dto/request/CreateTraceabilityRequest.java
@@ -0,0 +1,26 @@
+package com.pragma.microservicetraceabilityfoodcourt.application.dto.request;
+
+import com.pragma.microservicetraceabilityfoodcourt.application.constant.TraceabilityReqConstant;
+import com.pragma.microservicetraceabilityfoodcourt.domain.model.enums.OrderStatus;
+
+import javax.validation.constraints.*;
+import java.time.LocalDate;
+
+public record CreateTraceabilityRequest(
+        @NotNull(message = TraceabilityReqConstant.MSG_ORDER_ID_IS_POSITIVE)
+        @Positive
+        Long orderId,
+
+        @NotBlank(message = TraceabilityReqConstant.MSG_CLIENT_ID_IS_REQUIRED)
+        String clientId,
+
+        @NotBlank(message = TraceabilityReqConstant.MSG_CLIENT_EMAIL_IS_REQUIRED)
+        @Email(message = TraceabilityReqConstant.MSG_CLIENT_EMAIL_FORMAT)
+        String clientEmail,
+
+        @PastOrPresent
+        LocalDate date,
+
+        OrderStatus newStatus
+) {
+}
