3438bbef8bec1a2f843e1f5c371524325a862ef4
Nicolás Olmos <nicolas.olmos@pragma.com.co>
2024-07-15 09:33:37 -0500

    feat[HU-17(traceability)] add a builder for model class

---
+++ src/main/java/com/pragma/microservicetraceabilityfoodcourt/domain/builder/TraceabilityBuilder.java
@@ -0,0 +1,67 @@
+package com.pragma.microservicetraceabilityfoodcourt.domain.builder;
+
+import com.pragma.microservicetraceabilityfoodcourt.domain.enums.OrderStatus;
+import com.pragma.microservicetraceabilityfoodcourt.domain.model.Traceability;
+
+import java.time.LocalDate;
+
+public class TraceabilityBuilder {
+    private LocalDate date;
+    private String employeeId;
+    private String id;
+    private Long orderId;
+    private OrderStatus lastStatus;
+    private OrderStatus newStatus;
+    private String clientEmail;
+    private String clientId;
+    private String employeeEmail;
+
+    public TraceabilityBuilder setDate(LocalDate date) {
+        this.date = date;
+        return this;
+    }
+
+    public TraceabilityBuilder setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+        return this;
+    }
+
+    public TraceabilityBuilder setId(String id) {
+        this.id = id;
+        return this;
+    }
+
+    public TraceabilityBuilder setOrderId(Long orderId) {
+        this.orderId = orderId;
+        return this;
+    }
+
+    public TraceabilityBuilder setLastStatus(OrderStatus lastStatus) {
+        this.lastStatus = lastStatus;
+        return this;
+    }
+
+    public TraceabilityBuilder setNewStatus(OrderStatus newStatus) {
+        this.newStatus = newStatus;
+        return this;
+    }
+
+    public TraceabilityBuilder setClientEmail(String clientEmail) {
+        this.clientEmail = clientEmail;
+        return this;
+    }
+
+    public TraceabilityBuilder setClientId(String clientId) {
+        this.clientId = clientId;
+        return this;
+    }
+
+    public TraceabilityBuilder setEmployeeEmail(String employeeEmail) {
+        this.employeeEmail = employeeEmail;
+        return this;
+    }
+
+    public Traceability createTraceability() {
+        return new Traceability(date, employeeId, id, orderId, lastStatus, newStatus, clientEmail, clientId, employeeEmail);
+    }
+}
